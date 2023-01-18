9ab3321c7a832a6ed191b78587a03851dd43dd25
deepak-ega143 <deepakegastu@gmail.com>
2023-01-18 23:23:21 +0530

    ADD:Done

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import styled from "styled-components"
+import Fade from "react-reveal/Fade"
+
 function Section({title, description, leftBtnText, rightBtnText , backgroundImg}) {
   return (
 	     <Wrap bgImage={backgroundImg} >
-	        <ItemText>
-            <h1>{title}</h1>
-            <p>{description}</p>
-           </ItemText>
+         <Fade bottom>
+	            <ItemText>
+                <h1>{title}</h1>
+                <p>{description}</p>
+              </ItemText>
+           </Fade>
            <Buttons>
+            <Fade bottom>
            <ButtonGroup>
             <LeftButton>
               {leftBtnText}
@@ -18,7 +23,9 @@ function Section({title, description, leftBtnText, rightBtnText , backgroundImg}
             </RightButton>
              }
            </ButtonGroup>
+           
            <DownArrow src="/images/down-arrow.svg" />
+           </Fade>
            </Buttons>
 	     </Wrap>
   )
